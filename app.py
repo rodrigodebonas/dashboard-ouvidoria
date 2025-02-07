645e3e63f95ede77f745db38523be59bee3ac575
rodrigodebonas <92576042+rodrigodebonas@users.noreply.github.com>
2025-02-07 17:58:55 -0300

    Update app.py

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ from dash.dependencies import Input, Output, State
 from flask import Flask
 
 # 🔹 1. VARIÁVEIS GLOBAIS 🔹
-url = "https://dadosabertos-download.cgu.gov.br/e-Ouv/manifestacoes-ouvidoria.csv"
+url = "https://www.dropbox.com/scl/fi/mnrue4bu7onf83canz1ci/manifestacoes_original.csv?rlkey=g930wemxtfjahz9akr4oq8d38&st=m3gjyjaz&dl=1"
 arquivo_original = "manifestacoes_original.csv"
 arquivo_utf8 = "manifestacoes_utf8.csv"
 ultima_atualizacao = ""
